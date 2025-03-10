4f75d4bc6d83b0e43e0cd6821b8c4c989509654d
adnankhan2 <waheed.adnan@yahoo.com>
2025-03-11 00:44:43 +0200

    thirdcommit

--- src/ComponentC.jsx
+++ src/ComponentC.jsx
@@ -1,8 +1,12 @@
 import ComponentD from "./ComponentD";
+import React, { useContext } from "react";
+import { UserContext } from "./ComponentA";
 function ComponentC() {
+  const user = useContext(UserContext);
   return (
     <div className="box">
       <h2>Component C</h2>
+      <h2>{`Hello again Mr.${user}`}</h2>
       <ComponentD />
     </div>
   );
